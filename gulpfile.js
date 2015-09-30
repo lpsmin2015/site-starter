7c9ff99e3a4d7e3cc7b79bf0c5acd6e7029fb450
laurent Beauvisage <laurentbeauvisage@gmail.com>
2015-09-30 11:42:00 +0200

    install browser sync + modification gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch('./src/assets/scss/**/*.scss', ['sass']);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+});
 
 // creation d'une nouvelle tache : 'sass'
 gulp.task('sass', function () {
     // définition de la tache
-    gulp.src('./src/assets/scss/**/*.scss') // définition du répertoire source
+    return gulp.src('./src/assets/scss/**/*.scss') // définition du répertoire source
         .pipe(sass().on('error', sass.logError)) // exéc Sass
-        .pipe(gulp.dest('./src/assets/css')); // écriture dans destination
+        .pipe(gulp.dest('./src/assets/css')) // écriture dans destination
+        .pipe(browserSync.stream());
 });
 
 // creation d'une nouvelle tache : 'sass:watch'
